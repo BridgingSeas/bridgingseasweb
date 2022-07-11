5c805aeb843206513946edccf4c239c9fb5f39fc
szheng31 <63608979+szheng31@users.noreply.github.com>
2022-07-10 22:57:01 -0400

    update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,126 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="header">
+    <a href="" class="logo"><img src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=224,fit=crop/m2Wgwzkj3GFqQPR0/Bridging-Seas-mePe9k6belSbvxM5.png"></a>
+    <div class="header-right">
+        <a href="members">Members</a>
+        <a href="partners">Our Partners</a>
+        <a href="getinvolved">Get Involved</a>
+        <a href="contact">Contact Us</a>
+        
+    </div>
+</div>
+
+<body>
+    <div class="banner">
+        <title>
+            penis
+        </title>
+    </div>
+</body>
+
+
+
+<div class="footer">
+    <div class="footer-left">
+        <a href="https://www.instagram.com/bridgingseas/"><img src="https://cdn2.iconfinder.com/data/icons/black-white-social-media/32/instagram_online_social_media_photo-256.png" width="50" height="50"></a>
+        <a href="https://www.facebook.com/bridgingseas/"><img src="https://cdn2.iconfinder.com/data/icons/black-white-social-media/32/online_social_media_facebook-256.png" width="50" height="50"></a>
+        <a href="https://twitter.com/BridgingSeas"><img src="https://cdn2.iconfinder.com/data/icons/black-white-social-media/32/twitter_online_social_media-256.png" width="50" height="50"></a>
+        <a href="https://www.tiktok.com/@bridgingseas?"><img src="https://cdn.discordapp.com/attachments/609426945603797003/995796880351051817/IMG_1641.png" width="50" height="50"></a>
+    </div>
+
+    <div class="footer-right">
+        <h1>Contact Us</h1>
+        <p>bridgingseasnyc@gmail.com</p>
+
+    </div>
+</div>
+
+
+<style>
+    .body {
+        padding-top: 1000px;
+        padding-bottom: 1000px;
+    }
+    .header {
+        overflow: hidden;
+        background-color: #d7a899;
+        padding: 20px 10px;
+        position: fixed;
+        top: 0%;
+        width: 100%;
+        
+    }
+
+    /* Style the header links */
+    .header a {
+    float: left;
+    color: black;
+    text-align: center;
+    padding: 12px;
+    text-decoration: none;
+    font-size: 18px;
+    line-height: 25px;
+    border-radius: 4px;
+    }
+
+    /* Style the logo link (notice that we set the same value of line-height and font-size to prevent the header to increase when the font gets bigger */
+    .header a.logo {
+        font-style: alegreya;
+        font-size: 25px;
+        font-weight: bold;
+    }
+
+    /* Change the background color on mouse-over */
+    .header a:hover {
+    background-color: #ddd;
+    color: black;
+    }
+
+    /* Style the active/current link*/
+    
+
+    /* Float the link section to the right */
+    .header-right {
+    float: right;
+    }
+
+    /* Add media queries for responsiveness - when the screen is 500px wide or less, stack the links on top of each other */
+    @media screen and (max-width: 500px) {
+    .header a {
+        float: none;
+        display: block;
+        text-align: left;
+    }
+    .header-right {
+        float: none;
+    }
+
+    }
+
+
+    .footer {
+        overflow: hidden;
+        background-color: #d7a899;
+        padding: 20px 0px;
+        position:fixed;
+        top: 80%;
+        width: 100%;
+    }
+
+    .footer-left {
+        float: left;
+        padding: 45px 50px;
+    }
+
+    .footer-right {
+        float: right;
+        
+        color: black;
+        text-align: center;
+        padding: 12px;
+        text-decoration: none;
+        font-size: 18px;
+        line-height: 25px;
+        border-radius: 4px;
+    }
+</style>
