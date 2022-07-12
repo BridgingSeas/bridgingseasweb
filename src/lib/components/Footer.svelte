ab4085f887855461fe8dcff83ff85598a329268b
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2022-07-12 09:55:42 -0400

    yas

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,45 +1,41 @@
 <div class="w-full bg-[#ffc8b6] py-10 px-8">
-	<div class="container mx-auto grid grid-cols-2">
+	<div class="container mx-auto grid grid-cols-1 gap-y-4 md:grid-cols-2">
 		<div>
-			<!-- left side -->
+			<!-- left side / top-->
 			<div class="flex gap-4">
 				<a href="https://www.instagram.com/bridgingseas/"
 					><img
 						src="https://cdn2.iconfinder.com/data/icons/black-white-social-media/32/instagram_online_social_media_photo-256.png"
-						width="50"
-						height="50"
 						alt="instagram"
+						class="h-12 w-12"
 					/></a
 				>
 				<a href="https://www.facebook.com/bridgingseas/"
 					><img
 						src="https://cdn2.iconfinder.com/data/icons/black-white-social-media/32/online_social_media_facebook-256.png"
-						width="50"
-						height="50"
 						alt="facebook"
+						class="h-12 w-12"
 					/></a
 				>
 				<a href="https://twitter.com/BridgingSeas"
 					><img
 						src="https://cdn2.iconfinder.com/data/icons/black-white-social-media/32/twitter_online_social_media-256.png"
-						width="50"
-						height="50"
 						alt="twitter"
+						class="h-12 w-12"
 					/></a
 				>
 				<a href="https://www.tiktok.com/@bridgingseas?"
 					><img
 						src="https://cdn.discordapp.com/attachments/609426945603797003/995796880351051817/IMG_1641.png"
-						width="50"
-						height="50"
 						alt="tiktok"
+						class="h-12 w-12"
 					/></a
 				>
 			</div>
 		</div>
 
 		<div>
-			<!-- right side -->
+			<!-- right side / bottom-->
 			<div>
 				<p class="font-bold">Contact Us</p>
 				<a href="mailto:bridgingseasnyc@gmail.com">bridgingseasnyc@gmail.com</a>
--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -16,19 +16,18 @@
 	</div>
 </div> -->
 
-<div class="relative">
-	<img class="min-h-[25vh] object-scale-down" alt="waves" src={waves} />
-	<div class="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]">
-		<p class="text-center font-serif text-4xl text-[#fffffa] md:text-5xl lg:text-6xl">
-			Bridging Seas
-		</p>
-		<p class="mt-4 text-center font-serif text-lg text-[#fffffa] md:text-2xl lg:text-3xl">
+<div
+	class="flex min-h-[50vh] items-center justify-center bg-[url('$lib/waves.png')] bg-cover bg-center"
+>
+	<div>
+		<p class="text-center font-serif text-5xl text-[#fffffa] sm:text-6xl">Bridging Seas</p>
+		<p class="mt-4 text-center font-serif text-2xl text-[#fffffa] sm:text-3xl">
 			Unifying Asian Diaspora
 		</p>
 
 		<a href="/getinvolved">
 			<div
-				class="mx-auto mt-10 max-w-fit rounded-md bg-[#e88073] px-8 py-2 text-xl text-[#fffffa] lg:mt-20"
+				class="mx-auto mt-10 max-w-fit rounded-md bg-[#e88073] px-8 py-2 text-xl text-sm text-[#fffffa] sm:mt-20 sm:text-base"
 			>
 				Get Involved
 			</div>
