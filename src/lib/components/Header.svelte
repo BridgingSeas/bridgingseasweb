2fc18b80ba7a44f4d7b5df429b9f673d207d523c
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2022-07-11 23:44:08 -0400

    omg

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,4 +1,8 @@
-<div class="sticky top-0 w-full bg-[#ffc8b6] py-4 z-50 drop-shadow-xl">
+<script>
+	import menuIcon from "$lib/hamburger.png"
+</script>
+
+<div class="sticky top-0 z-50 w-full bg-[#ffc8b6] py-4 drop-shadow-xl">
 	<div class="container mx-auto flex items-center">
 		<a href="/"
 			><img
@@ -9,13 +13,26 @@
 
 		<div class="flex-grow" />
 
-		<div class="flex gap-x-8">
 
-			<a class="hover:text-[#565656] hover:underline underline-offset-8" href="/">Home</a>
-			<a class="hover:text-[#565656] hover:underline underline-offset-8" href="/members">Members</a>
-			<a class="hover:text-[#565656] hover:underline underline-offset-8" href="/partners">Our Partners</a>
-			<a class="hover:text-[#565656] hover:underline underline-offset-8" href="/getinvolved">Get Involved</a>
-			<a class="hover:text-[#565656] hover:underline underline-offset-8" href="/contact">Contact Us</a>
+		<div class="md:hidden">
+			<button>
+			<img width="24" height="24" src={menuIcon} alt="open menu">
+			</button>
+		</div>
+
+
+		<div class="hidden md:flex md:gap-x-4 lg:gap-x-8"> <!-- for large screens -->
+			<a class="underline-offset-8 hover:text-[#565656] hover:underline" href="/">Home</a>
+			<a class="underline-offset-8 hover:text-[#565656] hover:underline" href="/members">Members</a>
+			<a class="underline-offset-8 hover:text-[#565656] hover:underline" href="/partners"
+				>Our Partners</a
+			>
+			<a class="underline-offset-8 hover:text-[#565656] hover:underline" href="/getinvolved"
+				>Get Involved</a
+			>
+			<a class="underline-offset-8 hover:text-[#565656] hover:underline" href="/contact"
+				>Contact Us</a
+			>
 		</div>
 	</div>
 </div>
