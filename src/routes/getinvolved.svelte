219b8db21c741b6818351c78b44ed62891c0a9f3
szheng31 <63608979+szheng31@users.noreply.github.com>
2022-07-13 22:37:23 -0400

    .

--- src/routes/getinvolved.svelte
+++ src/routes/getinvolved.svelte
@@ -1,1 +1,65 @@
-<div class="container">Get Involved</div>
+<div class=" pt-20 pb-20 bg-[#f7e3d2]">
+    <div class="container mb-20 grid grid-cols-1 gap-x-8 pt-20 xl:grid-cols-2">
+
+        <div>
+            <!-- left / top -->
+            <p class="font-serif text-7xl font-bold text-center">Pen Pal</p>
+    
+            <p class="pt-4 text-center">
+                Bridging Sea's first and main service is our Pen Pal service. 
+                The Pen Pal program matches students of the same ethnicity or culture from different parts of the world 
+                to get to know each other and become friends.
+            </p>
+
+            <a href="/getinvolved">
+                <div
+                    class="mx-auto mt-10 max-w-fit rounded-md bg-[#e88073] px-8 py-2 text-xl text-sm text-[#fffffa] sm:mt-20 sm:text-base"
+                >
+                    Learn More
+                </div>
+            </a>
+            
+        </div>
+        
+        
+    
+        <div class="pt-10 xl:pt-0">
+            <!-- right / bottom -->
+            
+        </div>
+
+        
+    </div>
+    
+    
+</div>
+<div class=" pt-20 pb-20 bg-[#e3d1cc]">
+    <div class="container mb-20 grid grid-cols-1 gap-x-8 pt-20 xl:grid-cols-2">
+        
+    
+        <div class="pt-10 xl:pt-0">
+            <!-- right / bottom -->
+            
+        </div>
+        <div>
+            <!-- left / top -->
+            <p class="font-serif text-7xl font-bold text-center">Join the team</p>
+    
+            <p class="pt-4 text-center">
+                If you share the same sentiments as we do, consider joining the crew!
+                 We are always looking for passionate people who believe in our cause. 
+                All are welcome. Yes, including you.
+            </p>
+
+            <a href="https://x3vswh56e67.typeform.com/to/OqTPK0Nw">
+                <div
+                    class="mx-auto mt-10 max-w-fit rounded-md bg-[#e88073] px-8 py-2 text-xl text-sm text-[#fffffa] sm:mt-20 sm:text-base"
+                >
+                    Learn More
+                </div>
+            </a>
+        </div>
+    </div>
+    
+    
+</div>
--- src/routes/partners.svelte
+++ src/routes/partners.svelte
@@ -1,1 +1,11 @@
-<div class="container">Partners</div>
+<div class="mb-20 grid grid-cols-4 gap-x-8 xl:grid-cols-4">
+    <div class=" bg-[#f7e3d2] ">
+        hi
+    </div>
+
+    <div>
+
+    </div>
+
+    
+</div>
