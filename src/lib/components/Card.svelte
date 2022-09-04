6205f32f4208b4bd6613bdb2d1bc06f6a93922ad
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2022-09-03 22:18:44 -0400

    lolz

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -37,13 +37,24 @@
   }
   
   /* Position the front and back side */
-  .flip-card-front, .flip-card-back {
-    position: absolute;
+  .flip-card-front {
     width: 100%;
     height: 100%;
     -webkit-backface-visibility: hidden; /* Safari */
     backface-visibility: hidden;
   }
+
+
+  /* Position the front and back side */
+  .flip-card-back {
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    -webkit-backface-visibility: hidden; /* Safari */
+    backface-visibility: hidden;
+  }
+
   
   
   .flip-card-back {
