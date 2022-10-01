6e3eb43ad37293dbd57dc0cc91652c7463bb34eb
szheng31 <63608979+szheng31@users.noreply.github.com>
2022-09-30 23:22:08 -0400

    font changes and added story

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -27,12 +27,22 @@
 </div>
 -->
 <title>About Us</title>
+<div class="bg-[#FDE1D5]">
+	<div class="container flex items-center justify-center pt-10 pb-20 ">
+		<div class="text-center">
+			<p class="pb-8 text-8xl font-serif">Our Story</p>
+			<p class="text-[23px] font-semibold  mr-20 ml-20 leading-12">Founded in May of 2021 by three Chinese American high school students in New York City, Bridging Seas was created to do exactly what the name says: Bridge the seas. Like many immigrant students, our founders, Lauren Lee, Isabella Jia, and Sharon Liu were eager to feel closer to their cultural roots. As a result of the pandemic baring people around the world to travel and experience the benefits of cultural exchange, our founders decided to bring cultural exchange straight to students themselves. In particular, Bridging Seas aims to bridge the vast Asian diaspora because it is clear to us the collective struggle Asian students face in learning to accept their cultures when Western culture is so dominant globally. With an incredible team of young, brilliant students, Bridging Seas is proud to be a youth-led global organization that is fulfilling the purpose of its original founding.</p>
+		</div>
+	</div>
+</div>
+
 <div class="flex items-center justify-center pt-10 pb-20 ">
 	<div class="text-center">
 		<p class="pb-5 text-8xl font-serif">Our Founders</p>
 		<p class="text-2xl font-semibold">Meet the people who built Bridging Seas from the ground up.</p>
 	</div>
 </div>
+
 
 <div class="container grid grid-cols-1 gap-x-8 xl:grid-cols-3 pb-20">
 	<div>
