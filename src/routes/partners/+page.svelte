8b0d5ed81def95cdb9724830014e85fa405a1c51
Lauren Lee <laurenlee@mickey-6.local>
2024-11-03 20:14:19 -0500

    add analytics

--- src/routes/partners/+page.svelte
+++ src/routes/partners/+page.svelte
@@ -1,3 +1,12 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9NHVYZVMZV"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-9NHVYZVMZV');
+</script>
 <script>
 	import balloon from '$lib/img/logos/partners/balloon.png?w=500;700;896&webp&srcset';
 	import teens from '$lib/img/logos/partners/teens.png?w=500;700;896&webp&srcset';
