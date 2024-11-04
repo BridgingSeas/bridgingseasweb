52351bffb09d6f1d93351421736cac2cbac94bc3
Lauren Lee <laurenlee@mickey-6.local>
2024-11-03 20:14:19 -0500

    add analytics

--- src/routes/getinvolved/+page.svelte
+++ src/routes/getinvolved/+page.svelte
@@ -1,3 +1,12 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9NHVYZVMZV"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-9NHVYZVMZV');
+</script>
 <script>
 	import penpal from '$lib/img/graphics/penpal2.png?w=500;700;896&webp&srcset';
 	import team from '$lib/img/graphics/jointheteam.png?w=500;700;896&webp&srcset';
