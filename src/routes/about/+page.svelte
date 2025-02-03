d97a01b15d0cb3d6e6a8ca980c5718e809a223f4
Lauren Lee <laurenlee@Laurens-MacBook-Pro.local>
2025-02-03 17:09:12 -0500

    about me

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -17,27 +17,44 @@
 	import mai from '$lib/img/team/mai.jpg?w=500;300;896&webp&srcset';
 	import chloe from '$lib/img/team/chloe.jpg?w=500;800;896&webp&srcset';
 	import annie from '$lib/img/team/annie.png?w=500;700;896&webp&srcset';
+	import arya from '$lib/img/team/arya.jpg?w=500;700;896&webp&srcset';
+	import eric from '$lib/img/team/eric.jpeg?w=500;700;896&webp&srcset';
+	import erika from '$lib/img/team/erika.jpg?w=500;700;896&webp&srcset';
+	import geunyoung from '$lib/img/team/geunyoung.jpg?w=500;700;896&webp&srcset';
+	import kaitlyn from '$lib/img/team/kaitlyn.jpeg?w=500;700;896&webp&srcset';
+	import klarisse from '$lib/img/team/klarisse.jpeg?w=500;700;896&webp&srcset';
+	import lixin from '$lib/img/team/lixin.png?w=500;700;896&webp&srcset';
+	import sarika from '$lib/img/team/sarika 2.jpg?w=500;700;896&webp&srcset';
+	import shiza from '$lib/img/team/shiza.jpg?w=500;700;896&webp&srcset';
+	import shreya from '$lib/img/team/shreya.jpg?w=500;700;896&webp&srcset';
+	import shweta from '$lib/img/team/shweta 2.jpg?w=500;700;896&webp&srcset';
+	import leanna from '$lib/img/team/leanna.png?w=500;700;896&webp&srcset';
+	import ngan from '$lib/img/team/ngann.jpg?w=500;700;896&webp&srcset';
+	import yeonjae from '$lib/img/team/yeonjae.jpg?w=500;700;896&webp&srcset';
+	import ishaa from '$lib/img/team/9DBF3F40-99AF-4495-B042-165F6BC8D986.jpg?w=500;500;896&webp&srcset';
+
 </script>
 
 <title>About Us</title>
 <div class="bg-[#FDE1D5]">
-	<div class="container flex items-center justify-center pt-10 pb-20 ">
+	<div class="container flex items-center justify-center pt-8 pb-20 ">
 		<div class="text-center">
-			<p class="pb-8 text-8xl font-serif">Our Story</p>
-			<p class="text-[23px] font-semibold  mr-20 ml-20 leading-12">Founded in May of 2021 by three Chinese American high school students in New York City, Bridging Seas was created to do exactly what the name says: Bridge the seas. Like many immigrant students, our founders, Lauren Lee, Isabella Jia, and Sharon Liu were eager to feel closer to their cultural roots. As a result of the pandemic baring people around the world to travel and experience the benefits of cultural exchange, our founders decided to bring cultural exchange straight to students themselves. In particular, Bridging Seas aims to bridge the vast Asian diaspora because it is clear to us the collective struggle Asian students face in learning to accept their cultures when Western culture is so dominant globally. With an incredible team of young, brilliant students, Bridging Seas is proud to be a youth-led global organization that is fulfilling the purpose of its original founding.</p>
+			<p class="pb-8 text-7xl font-serif">Our Story</p>
+			<p class="text-[18px] font-semibold  mr-20 ml-20 leading-12">
+				Founded in May of 2021 by three Chinese American high school students in New York City, Bridging Seas was created to do exactly what the name says: Bridge the seas. Like many immigrant students, our founders, Lauren Lee, Isabella Jia, and Sharon Liu, were eager to feel closer to their cultural roots. As a result of the pandemic barring people around the world from traveling and experiencing the benefits of cultural exchange, our founders decided to bring cultural exchange straight to students themselves. In particular, Bridging Seas aims to bridge the vast Asian diaspora because it is clear to us the collective struggle Asian students face in learning to accept their cultures when Western culture is so dominant globally. Today, Bridging Seas remains largely high school-led, with a strong emphasis on leadership development, providing students with the opportunity to take ownership of the program and actively engage in their cultural education. With an incredible team of young, brilliant students, Bridging Seas is proud to be a youth-led global organization that is fulfilling the purpose of its original founding.</p>
 		</div>
 	</div>
 </div>
 
 <div class="flex items-center justify-center pt-10 pb-20 ">
 	<div class="text-center">
-		<p class="pb-5 text-8xl font-serif">Our Founders</p>
+		<p class="pb-5 text-7xl font-serif">Our Founders</p>
 		<p class="text-2xl font-semibold">Meet the people who built Bridging Seas from the ground up.</p>
 	</div>
 </div>
 
 
-<div class="container grid grid-cols-1 gap-x-8 xl:grid-cols-3 pb-20">
+<div class="container grid grid-cols-1 gap-x-8 xl:grid-cols-2 pb-19">
 	<div>
 		<img type="image/webp" srcset={lauren} alt="lauren" height="1000" width="1000" />
 		<p class="pt-4 font-serif text-center text-4xl">Lauren Lee</p>
@@ -53,22 +70,20 @@
 			<p class="pt-4 pb-2 text-center text-xl font-semibold">Co-Founder</p>
 		</div>
 	</div>
-
-	<div>
-		<img type="image/webp" srcset={izzy} alt="isabella" />
-		<p class="pt-4 font-serif text-center text-4xl">Isabella Jia</p>
-		<div>
-			<p class="pt-4 pb-2 text-center text-xl font-semibold">Co-Founder</p>
-		</div>
-	</div>
 </div>
 
 
 
-<div class="flex items-center justify-center pt-10 pb-20 ">
+<div class="flex items-center justify-center pt-10 pb-15 ">
 	<div class="text-center">
 		<p class="pb-5 text-8xl font-serif">Our Directors</p>
 		<p class="text-2xl font-semibold">Meet the people who keep Bridging Seas awesome.</p>
+	</div>
+</div>
+
+<div class="flex items-center justify-center pt-10 pb-20 ">
+	<div class="text-center">
+		<p class="pb-5 text-7xl font-serif">American Region</p>
 	</div>
 </div>
 
@@ -77,40 +92,40 @@
 	<div>
 		<img type="image/webp" srcset={mai} alt="Mai" height="1000" width="1000" />
 		<p class="pt-4 font-serif text-center text-4xl">Mai Tran</p>
-		<p class="pt-4 font-semibold pb-2 text-center text-xl">Executive Director</p>
-		<p class="pb-4 text-center text-xl">Senior at Stuyvesant High School</p>
+		<p class="pt-4 font-semibold pb-2 text-center text-xl">American Regional Lead</p>
+		<p class="pb-4 text-center text-xl">Stuyvesant High School</p>
 	</div>
 
 	<div>
 		<img type="image/webp" srcset={chloe} alt="Chloe" height="1000" width="1000" />
 		<p class="pt-4 font-serif text-center text-4xl">Chloe Kan</p>
-		<p class="pt-4 font-semibold pb-2 text-center text-xl">Executive Director</p>
-		<p class="pb-4 text-center text-xl">Senior at Stuyvesant High School</p>
+		<p class="pt-4 font-semibold pb-2 text-center text-xl">American Regional Lead</p>
+		<p class="pb-4 text-center text-xl">Stuyvesant High School</p>
 	</div>
 
 	<div>
-		<img type="image/webp" srcset={annie} alt="Raymond" height="1000" width="1000" />
-		<p class="pt-4 font-serif text-center text-4xl">Annie Li</p>
-		<p class="pt-4 font-semibold pb-2 text-center text-xl">Director of Curriculum</p>
-		<p class="pb-4 text-center text-xl">Junior at Stuyvesant High School</p>
+		<img type="image/webp" srcset={eric} alt="Eric" height="1000" width="1000" />
+		<p class="pt-4 font-serif text-center text-4xl">Eric Lam</p>
+		<p class="pt-4 font-semibold pb-2 text-center text-xl">Director of Chinese Outreach</p>
+		<p class="pb-4 text-center text-xl">Stuyvesant High School</p>
 	</div>
 
 </div>
 
 <div class="container grid grid-cols-1 gap-x-8 xl:grid-cols-3 pb-20">
-	<!--<div>
-	<img type="image/webp" srcset={raymond} alt="Raymond" height="1000" width="1000" />
-		<p class="pt-4 font-serif text-center text-4xl">Raymond Lin</p>
-		<p class="pt-4 font-semibold pb-2 text-center text-xl">Director of Technology</p>
-		<p class="pb-4 text-center text-xl">Junior at Stuyvesant High School</p> 
-	</div>-->
-
 
 	<div>
-		<img type="image/webp" srcset={jenny} alt="Jenny" height="1000" width="1000" />
-		<p class="pt-4 font-serif text-center text-4xl">Jenny Tan</p>
-		<p class="pt-4 font-semibold pb-2 text-center text-xl">Director of Social Media</p>
-		<p class="pb-4 text-center text-xl">Senior at Stuyvesant High School</p>
+		<img type="image/webp" srcset={lixin} alt="Lixin" height="1000" width="1000" />
+		<p class="pt-4 font-serif text-center text-4xl">Lixin Zhang</p>
+		<p class="pt-4 font-semibold pb-2 text-center text-xl">Director of Graphic Design</p>
+		<p class="pb-4 text-center text-xl">Stuyvesant High School</p>
+	</div>
+
+	<div>
+		<img type="image/webp" srcset={sarika} alt="Sarika" height="1000" width="1000" />
+		<p class="pt-4 font-serif text-center text-4xl">Sarika Degtyarev</p>
+		<p class="pt-4 font-semibold pb-2 text-center text-xl">Chapter Manager</p>
+		<p class="pb-4 text-center text-xl">Mission Vista High School</p>
 	</div>
 
 	<div>
@@ -121,5 +136,139 @@
 	</div>
 </div>
 
+<div class="container grid grid-cols-1 gap-x-8 xl:grid-cols-3 pb-15">
+
+	<div>
+		<img type="image/webp" srcset={leanna} alt="Leanna" height="1000" width="1000" />
+		<p class="pt-4 font-serif text-center text-4xl">Leanna Lin</p>
+		<p class="pt-4 font-semibold pb-2 text-center text-xl">Treasurer</p>
+		<p class="pb-4 text-center text-xl">Stuyvesant High School</p>
+	</div>
+<!-- 
+	<div>
+		<img type="image/webp" srcset={ishaa} alt="Isha" height="700" width="700"/>
+		<p class="pt-4 font-serif text-center text-4xl">Shahana Ferdous</p>
+		<p class="pt-4 font-semibold pb-2 text-center text-xl">Director of Bangladeshi Outreach</p>
+		<p class="pb-4 text-center text-xl">Senior at Stuyvesant High School</p>
+	</div> -->
+
+
+</div>
+
+<div class="container grid grid-cols-1 gap-x-8 xl:grid-cols-3 pb-15">
+</div>
+
+<div class="flex items-center justify-center pt-10 pb-20 ">
+	<div class="text-center">
+		<p class="pb-5 text-7xl font-serif">Singaporean Region</p>
+	</div>
+</div>
+
+<div class="container grid grid-cols-1 gap-x-8 xl:grid-cols-3 pb-15">
+
+	<div>
+		<img type="image/webp" srcset={kaitlyn} alt="Kaitlyn" height="1000" width="1000" />
+		<p class="pt-4 font-serif text-center text-4xl">Kaitlyn Tay</p>
+		<p class="pt-4 font-semibold pb-2 text-center text-xl">Singaporean Regional Lead</p>
+		<p class="pb-4 text-center text-xl">Raffles Girls' School</p>
+	</div>
+	<div>
+		<img type="image/webp" srcset={klarisse} alt="Klarrise" height="1000" width="1000" />
+		<p class="pt-4 font-serif text-center text-4xl">Klarisse Ng</p>
+		<p class="pt-4 font-semibold pb-2 text-center text-xl">Singaporean Regional Lead</p>
+		<p class="pb-4 text-center text-xl">Raffles Girls' School</p>
+	</div>
+
+</div>
+
+<div class="flex items-center justify-center pt-10 pb-20 ">
+	<div class="text-center">
+		<p class="pb-5 text-7xl font-serif">Canadian Region</p>
+	</div>
+</div>
+<div class="container grid grid-cols-1 gap-x-8 xl:grid-cols-3 pb-15">
+
+	<div>
+		<img type="image/webp" srcset={erika} alt="Erika" height="1000" width="1000" />
+		<p class="pt-4 font-serif text-center text-4xl">Erika Puri</p>
+		<p class="pt-4 font-semibold pb-2 text-center text-xl">Canadian Regional Lead</p>
+		<p class="pb-4 text-center text-xl">Bishop P.F. Reding</p>
+	</div>
+	<div>
+		<img type="image/webp" srcset={shweta} alt="Shweta" height="1000" width="1000" />
+		<p class="pt-4 font-serif text-center text-4xl">Shweta Saju</p>
+		<p class="pt-4 font-semibold pb-2 text-center text-xl">Canadian Regional Lead</p>
+		<p class="pb-4 text-center text-xl">Bishop P.F. Reding</p>
+	</div>
+	<div>
+		<img type="image/webp" srcset={shiza} alt="Shiza" height="1000" width="1000" />
+		<p class="pt-4 font-serif text-center text-4xl">Shiza Khan</p>
+		<p class="pt-4 font-semibold pb-2 text-center text-xl">Director of Graphic Design</p>
+		<p class="pb-4 text-center text-xl">St. John's College</p>
+	</div>
+
+</div>
+
+<div class="flex items-center justify-center pt-10 pb-20 ">
+	<div class="text-center">
+		<p class="pb-5 text-7xl font-serif">Indian Region</p>
+	</div>
+</div>
+
+<div class="container grid grid-cols-1 gap-x-8 xl:grid-cols-3 pb-15">
+
+	<div>
+		<img type="image/webp" srcset={arya} alt="Arya" height="1000" width="1000" />
+		<p class="pt-4 font-serif text-center text-4xl">Arya Nair</p>
+		<p class="pt-4 font-semibold pb-2 text-center text-xl">Indian Regional Lead</p>
+		<p class="pb-4 text-center text-xl">Oakridge International School</p>
+	</div>
+	<div>
+		<img type="image/webp" srcset={shreya} alt="Shreya" height="1000" width="1000" />
+		<p class="pt-4 font-serif text-center text-4xl">Shreya Bhosale</p>
+		<p class="pt-4 font-semibold pb-2 text-center text-xl">Indian Regional Lead</p>
+		<p class="pb-4 text-center text-xl">Cambridge International School</p>
+	</div>
+
+</div>
+
+<div class="flex items-center justify-center pt-10 pb-20 ">
+	<div class="text-center">
+		<p class="pb-5 text-7xl font-serif">Korean Region</p>
+	</div>
+</div>
+
+<div class="container grid grid-cols-1 gap-x-8 xl:grid-cols-3 pb-15">
+
+	<div>
+		<img type="image/webp" srcset={yeonjae} alt="Yeonjae" height="1000" width="1000" />
+		<p class="pt-4 font-serif text-center text-4xl">Yeonjae Kim</p>
+		<p class="pt-4 font-semibold pb-2 text-center text-xl">Korean Regional Lead</p>
+		<p class="pb-4 text-center text-xl">Branksome Hall Asia</p>
+	</div>
+	<div>
+		<img type="image/webp" srcset={geunyoung} alt="Geunyoung" height="1000" width="1000" />
+		<p class="pt-4 font-serif text-center text-4xl">Geunyong Lee</p>
+		<p class="pt-4 font-semibold pb-2 text-center text-xl">Chapter Manager</p>
+		<p class="pb-4 text-center text-xl">Branksome Hall Asia</p>
+	</div>
+
+
+</div>
+
+<div class="flex items-center justify-center pt-10 pb-20 ">
+	<div class="text-center">
+		<p class="pb-5 text-7xl font-serif">Australian Region</p>
+	</div>
+</div>
+
 <div class="container grid grid-cols-1 gap-x-8 xl:grid-cols-3 pb-20">
+
+	<div>
+		<img type="image/webp" srcset={ngan} alt="Ngan" height="1000" width="1000" />
+		<p class="pt-4 font-serif text-center text-4xl">Ngan Nguyen</p>
+		<p class="pt-4 font-semibold pb-2 text-center text-xl">Australian Regional Lead</p>
+		<p class="pb-4 text-center text-xl"></p>
+	</div>
+
 </div>
